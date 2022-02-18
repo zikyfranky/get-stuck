fa2496f009e9be3eebd43caac722a18d4dcb8c7b
Isaac Frank <codingwithisaac@gmail.com>
2022-02-19 00:58:37 +0100

    make functions ownable

--- utils/GetStuck.sol
+++ utils/GetStuck.sol
@@ -3,13 +3,18 @@
 pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract GetStuck {
+contract GetStuck is Ownable {
     /// @notice Get stuck tokens in the contract
     /// @dev Function can be overriden
     /// @param token The address of the token stuck in the contract
     /// @param receiver The address of who would receive the stuck token
-    function getStuckToken(IERC20 token, address receiver) public virtual {
+    function getStuckToken(IERC20 token, address receiver)
+        public
+        virtual
+        onlyOwner
+    {
         uint256 amount = token.balanceOf(address(this));
         token.transfer(receiver, amount);
     }
@@ -17,7 +22,7 @@ contract GetStuck {
     /// @notice Get stuck eth in the contract
     /// @dev Function can be overriden
     /// @param receiver The address of who would receive the stuck eth
-    function getStuckETH(address receiver) public virtual {
+    function getStuckETH(address receiver) public virtual onlyOwner {
         uint256 amount = address(this).balance;
         (bool success, bytes memory data) = payable(receiver).call{
             value: amount
